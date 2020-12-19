5c29c156f88fbc14de58a5ed04d0e44e43b0d604
Husniye <husniyesakmak.91@gmail.com>
2020-12-19 10:44:20 +0100

    project modified

--- main.js
+++ main.js
@@ -54,13 +54,27 @@ for (let primeNumber = 2; primeNumber < MAX_BOUNDARY; primeNumber++) {
 const NUMBER_OF_PASSWORD = 50;
 const PASSWORD_MAX = 8;
 const PASSWORD_MIN = 16;
-const charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz.!'^+%&/()=?_";
+const charsetLetter = "abcdefghiklmnopqrstuvwxyz";
+const charsetLetterCapital="ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+const charset= ".!'^+%&/*()=?_";
+const charsetNumber= "0123456789";
 for (let index = 0; index < NUMBER_OF_PASSWORD; index++) {
     let passwordLength = Math.floor(Math.random() * (PASSWORD_MAX - PASSWORD_MIN)) + PASSWORD_MIN;
-    let randomPassword = '';
-for (let passwordIndex = 0; passwordIndex < passwordLength; passwordIndex++) {
-    let randomNumber = Math.floor(Math.random() * charset.length);
-    randomPassword += charset.substr(randomNumber,1);
+    let randomPassword ="";
+for (let passwordIndex = 1; passwordIndex < passwordLength; passwordIndex++) {
+    if (passwordIndex % 3 == 0) {
+        let randomNumber = Math.floor(Math.random() * charset.length);
+        randomPassword += charset.charAt(randomNumber)
+    } else if (passwordIndex % 4 == 0) {
+        let randomNumber = Math.floor(Math.random() * charsetLetterCapital.length);
+        randomPassword += charsetLetterCapital.charAt(randomNumber)
+    } else if (passwordIndex % 5 == 0) {
+        let randomNumber = Math.floor(Math.random() * charsetNumber.length);
+        randomPassword += charsetNumber.charAt(randomNumber)
+    } else {
+        let randomNumber = Math.floor(Math.random() * charsetLetter.length);
+        randomPassword += charsetLetter.charAt(randomNumber)
+    }
 }
 console.log(`Password: ${randomPassword}`);
 }
@@ -73,7 +87,33 @@ for (let startIndex = 0; startIndex < MAX_STAR; startIndex++) {
     starList.push(YILDIZ)
     console.log(starList.join(""))
 }
-
+let sifreler = [];
+const kucukHarfler = "abcdefghijklmnopqrstuvwxyz";
+const buyukHarfler = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const rakamlar = "0123456789"
+const kareterler = "!@#$%^&*()_+~`|}{[]\:;?><,./-="
+const sifreSayisi = 50;
+for (ilkDongu = 0; ilkDongu < sifreSayisi; ilkDongu++) {
+    const sifreUzunlugu = Math.floor(Math.random() * 8) + 8
+    let sifre = " ";
+    for (index = 1; index < sifreUzunlugu; index++) {
+        if (index % 3 == 0) {
+            let karakterSec = Math.floor(Math.random() * buyukHarfler.length);
+            sifre += buyukHarfler.charAt(karakterSec)
+        } else if (index % 5 == 0) {
+            let karakterSec = Math.floor(Math.random() * rakamlar.length);
+            sifre += rakamlar.charAt(karakterSec)
+        } else if (index % 4 == 0) {
+            let karakterSec = Math.floor(Math.random() * kareterler.length);
+            sifre += kareterler.charAt(karakterSec)
+        } else {
+            let karakterSec = Math.floor(Math.random() * kucukHarfler.length);
+            sifre += kucukHarfler.charAt(karakterSec)
+        }
+    }
+    sifreler.push(sifre)
+}
+console.log("50 Adet Sifre:", sifreler)
 
 
 
